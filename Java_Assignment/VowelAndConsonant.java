327236fb10d26db31d787b1fe80d3bb214da1b85
Bamzy123 <stephenomotos@gmail.com>
2024-11-02 00:00:53 +0100

    nokia

--- Java_Assignment/VowelAndConsonant.java
+++ Java_Assignment/VowelAndConsonant.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class VowelAndConsonant{
 	public static void main(String... args) {
+
 		Scanner input = new Scanner(System.in);
 
 		System.out.print("enter a letter: ");
