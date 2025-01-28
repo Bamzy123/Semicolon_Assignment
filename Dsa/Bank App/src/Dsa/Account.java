bf086acb048ff87862b5002907afd4358d72699f
Bamzy123 <stephenomotos@gmail.com>
2025-01-28 08:43:02 +0100

    Bank App

---
+++ Dsa/Bank App/src/Dsa/Account.java
@@ -0,0 +1,46 @@
+package Dsa;
+
+public class Account {
+    private int balance;
+    private boolean itExist;
+    private String pin;
+    private String name;
+
+    public Account(String pin, String name) {
+        this.balance = 0;
+        this.itExist = true;
+        this.pin = pin;
+        this.name = name;
+    }
+
+    public boolean itExist()
+    {
+        return itExist;
+    }
+    public boolean notExist() {
+        itExist = false;
+        return false;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void deposit(int amount) {
+        if (amount > 0) balance += amount;
+        else throw new IllegalArgumentException("Deposit amount must be greater than 0");
+    }
+
+    public void withdraw(int amount, String inputPin) {
+        if (!inputPin.equals(pin)) {
+            throw new SecurityException("Invalid PIN");
+        }
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Withdrawal amount must be greater than 0");
+        }
+        if (amount > balance) {
+            throw new IllegalArgumentException("Insufficient balance");
+        }
+        balance -= amount;
+    }
+}
---
+++ Dsa/Bank App/test/Dsa/AccountTest.java
@@ -0,0 +1,66 @@
+package Dsa;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AccountTest {
+
+    @Test
+    public void testThatAccountExist() {
+        String pin = "1234";
+        String name = "stephen";
+        Account account = new Account(pin, name);
+        assertTrue(account.itExist());
+    }
+
+    @Test
+    public void testThatAccountDoesNotExist() {
+        String pin = "1234";
+        String name = "stephen";
+        Account account = new Account(pin, name);
+        assertFalse(account.notExist());
+    }
+
+    @Test
+    public void testDeposit() {
+        String pin = "1234";
+        String name = "stephen";
+        Account account = new Account(pin, name);
+
+        assertEquals(0, account.getBalance());
+        account.deposit(200);
+        assertEquals(200, account.getBalance());
+
+    }
+
+    @Test
+    public void testForNegativeDeposit() {
+        String pin = "1234";
+        String name = "stephen";
+        Account account = new Account(pin, name);
+
+        Exception exception = null;
+        try {
+            account.deposit(-100);
+        } catch (IllegalArgumentException e) {
+            exception = e;
+        }
+        assertNotNull(exception);
+        assertTrue(exception.getMessage().contains("Deposit amount must be greater than 0"));
+    }
+
+    @Test
+    public void testWithdraw() {
+        String pin = "1234";
+        String name = "stephen";
+        Account account = new Account(pin, name);
+
+        assertEquals(0, account.getBalance());
+        account.deposit(200);
+        assertEquals(200, account.getBalance());
+
+        account.withdraw(100, pin);
+        assertEquals(100, account.getBalance());
+    }
+}
