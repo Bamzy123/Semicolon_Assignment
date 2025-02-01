a44031df0e4ab3744ba0b654634d50e7a3b3b49b
Bamzy123 <stephenomotos@gmail.com>
2025-02-01 01:03:20 +0100

    Diary App

--- Dsa/Diary App/src/Dsa/Diary.java
+++ Dsa/Diary App/src/Dsa/Diary.java
@@ -1,4 +1,26 @@
 package Dsa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Diary {
+    private String userName;
+    private String password;
+    private boolean isLocked;
+    private List<Entry> diary;
+
+    public Diary(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+        this.isLocked = true;
+        this.diary = new ArrayList<>();
+    }
+
+    public boolean isLocked() {
+        return isLocked;
+    }
+
+    public void unlockDiary(String inputPassword) {
+        if (this.password.equals(inputPassword)) this.isLocked = false;
+    }
 }
--- Dsa/Diary App/test/Dsa/DiaryTest.java
+++ Dsa/Diary App/test/Dsa/DiaryTest.java
@@ -1,4 +1,30 @@
 package Dsa;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class DiaryTest {
+
+    @Test
+    public void testDiaryCanBeCreated() {
+        Diary diary = new Diary("stephen", "password123");
+        assertNotNull(diary);
+    }
+
+    @Test
+    public void testThatDiaryIsLocked() {
+        Diary diary = new Diary("stephen", "password123");
+        assertTrue(diary.isLocked());
+    }
+
+    @Test
+    void testDiaryCanBeUnlockedWithCorrectPassword() {
+        Diary diary = new Diary("Bamsy", "password123");
+        diary.unlockDiary("password123");
+        assertFalse(diary.isLocked());
+    }
+
+    @Test
+
 }
