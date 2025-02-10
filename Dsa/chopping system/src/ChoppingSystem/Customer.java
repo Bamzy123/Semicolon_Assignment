dfb7fb0b83159ae4d5fe489ed9da7930b909c919
Bamzy123 <stephenomotos@gmail.com>
2025-02-10 16:22:47 +0100

    E_Store Application

--- Dsa/chopping system/src/ChoppingSystem/Customer.java
+++ Dsa/chopping system/src/ChoppingSystem/Customer.java
@@ -3,7 +3,7 @@ package ChoppingSystem;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Cutomer extends User {
+public class Customer extends User {
     private List<BillingInformation> billingInfos = new ArrayList<>();
     private ShoppingCart shoppingCart = new ShoppingCart();
 }
