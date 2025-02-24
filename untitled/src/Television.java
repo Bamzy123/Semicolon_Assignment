63b9f5fb0ab44a9ecaf484ccc499d30767b73e94
Bamzy123 <stephenomotos@gmail.com>
2025-02-24 21:08:50 +0100

    My Television

--- untitled/src/Television.java
+++ untitled/src/Television.java
@@ -1,6 +1,8 @@
 public class Television {
     private boolean power;
-    int increaseVolume = 0;
+    private int volume = 0;
+    private int channel = 1;
+    private boolean muted = true;
 
     public boolean power() {
         return power;
@@ -14,8 +16,44 @@ public class Television {
         power = false;
     }
 
-    public int increaseVolume() {
-        if (power) increaseVolume++;
-        return increaseVolume;
+    public int volume() {
+        return volume;
+    }
+
+    public void increaseVolume() {
+        if (power) if (volume < 100)
+                volume++;
+    }
+
+    public void decreaseVolume() {
+        if (power) if (volume > 0)
+            volume--;
+    }
+
+    public int channel() {
+        return channel;
+    }
+
+    public void channelUp() {
+        if (power) if (channel <= 100)
+            channel++;
+    }
+
+    public void channelDown() {
+        if (power) if (channel > 1)
+            channel--;
+    }
+
+    public void setChannel(int number) {
+        if (power) if (number <= 100)
+            channel = number;
+    }
+
+    public void mute() {
+        muted = power;
+    }
+
+    public boolean muted() {
+        return muted;
     }
 }
--- untitled/test/TelevisionTest.java
+++ untitled/test/TelevisionTest.java
@@ -35,7 +35,81 @@ public class TelevisionTest {
     public void testThatTelevisionCanIncreaseVolume() {
         television.isOn();
         assertTrue(television.power());
+        television.volume();
+        television.increaseVolume();
+        assertEquals(1, television.volume());
+    }
+
+    @Test
+    public void testThatTelevisionCanDecreaseVolume() {
+        television.isOn();
+        assertTrue(television.power());
+        television.volume();
+        television.decreaseVolume();
+        assertEquals(8, television.volume(), 10);
+    }
+
+    @Test
+    public void testThatVolumeDoesNotChangeWhenItIsOff() {
+        assertFalse(television.power());
+        television.volume();
 //        television.increaseVolume();
-        assertEquals(1, television.increaseVolume());
+        assertEquals(0, television.volume(), 10);
+    }
+
+    @Test
+    public void testThatChannelCanGoUp() {
+        television.isOn();
+        assertTrue(television.power());
+        television.channel();
+        television.channelUp();
+        assertEquals(2, television.channel());
+    }
+
+    @Test
+    public void testThatChannelCanGoDown() {
+        television.isOn();
+        assertTrue(television.power());
+        television.channel();
+        television.channelDown();
+        assertEquals(1, television.channel(), 5);
+    }
+
+    @Test
+    public void testThatChannelCannotChangeWhenItIsOff() {
+        assertFalse(television.power());
+        television.channel();
+//        television.channelUp();
+        assertEquals(0, television.channel(), 5);
+    }
+
+    @Test
+    public void testThatChannelCanBeSet() {
+        television.isOn();
+        assertTrue(television.power());
+        television.setChannel(13);
+        assertEquals(13, television.channel());
+    }
+
+    @Test
+    public void testThatSettingChannelShouldFailWhenItIsOff() {
+        assertFalse(television.power());
+        television.setChannel(13);
+        assertEquals(0, television.channel(), 13);
+    }
+
+    @Test
+    public void testThatTelevisionCanBeMute() {
+        television.isOn();
+        assertTrue(television.power());
+        television.mute();
+        assertTrue(television.muted());
+    }
+
+    @Test
+    public void testThatTelevisionCannotBeMutedWhenItIsOff() {
+        assertFalse(television.power());
+        television.mute();
+        assertFalse(television.muted());
     }
 }
